85ac444ef1d96f05e731efc8c916173467372c93
fauzandwip <fauzan27dp@gmail.com>
2024-07-25 23:15:19 +0700

    Initial Commit

---
+++ MyWishProducts/MyWishProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWishProductsApp.swift
+//  MyWishProducts
+//
+//  Created by Fauzan Dwi Prasetyo on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWishProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
